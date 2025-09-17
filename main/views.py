ace4b7cfdc9ade92cd0c91ee2c4a7eec85a96468
puut12 <hamidah.p2512@gmail.com>
2025-09-17 15:45:24 +0700

    tugas 4 on progress

--- main/views.py
+++ main/views.py
@@ -1,18 +1,30 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from main.forms import ItemsForm, ItemsSizeForm
 from main.models import Items
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.core import serializers
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+import datetime
+from django.urls import reverse
 
 # Create your views here.
+@login_required(login_url='/login')
 def show_main(request):
-    items_list = Items.objects.all()
+    filter_type = request.GET.get("filter", "all")
+    if filter_type == "all":
+        items_list = Items.objects.all()
+    else:
+        items_list = Items.objects.filter(user=request.user)
 
     context = {
         'app' : 'CS Corner',
-        'name': 'Putri Hamidah Riyanto',
+        'name': request.user.username,
         'class': 'PBP C',
-        'items_list': items_list
+        'items_list': items_list,
+        'last_login': request.COOKIES.get('last_login', 'Never')
     }
 
     return render(request, "main.html", context)
@@ -26,7 +38,10 @@ def create_items(request):
             form_class = ItemsSizeForm
         form = form_class(request.POST)
         if form.is_valid():
-            form.save()
+            # form.save()
+            items_entry = form.save(commit = False)
+            items_entry.user = request.user
+            items_entry.save()
             return redirect('main:show_main')
     else:
         form = ItemsForm()
@@ -34,6 +49,7 @@ def create_items(request):
     context = {'form': form}
     return render(request, "create_items.html", context)
 
+@login_required(login_url='/login')
 def show_items(request, id):
     items = get_object_or_404(Items, pk=id)
     items.increment_views()
@@ -69,3 +85,37 @@ def show_json_by_id(request, items_id):
         return HttpResponse(json_data, content_type="application/json")
     except Items.DoesNotExist:
        return HttpResponse(status=404)
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+   if request.method == 'POST':
+      form = AuthenticationForm(data=request.POST)
+
+      if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main"))
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+
+   else:
+      form = AuthenticationForm(request)
+   context = {'form': form}
+   return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
